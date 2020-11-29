44ee30a410f17a91568918a2a4e48d83c5dd597c
liujidong <593098937@qq.com>
2020-11-29 22:33:00 +0800

    基本架构

---
+++ countdown.js
@@ -0,0 +1,41 @@
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
+var RADIUS = 8;
+var MARGIN_TOP = 60;
+var MARGIN_LEFT = 30;
+
+window.onload = function(){
+
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+
+    render( context )
+}
+
+function render( cxt ){
+
+    var hours = 12
+    var minutes = 34
+    var seconds = 56
+
+    renderDigit( MARGIN_LEFT , MARGIN_TOP , parseInt(hours/10) , cxt )
+}
+
+function renderDigit( x , y , num , cxt ){
+
+    cxt.fillStyle = "rgb(0,102,153)";
+
+    for( var i = 0 ; i < digit[num].length ; i ++ )
+        for(var j = 0 ; j < digit[num][i].length ; j ++ )
+            if( digit[num][i][j] == 1 ){
+                cxt.beginPath();
+                cxt.arc()
+                cxt.closePath()
+
+                cxt.fill()
+            }
+}
+
