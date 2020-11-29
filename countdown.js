c0edd537c4e58f2922ba14081e3e4cd610c096f9
liujidong <593098937@qq.com>
2020-11-29 23:06:27 +0800

    动画基础函数

--- countdown.js
+++ countdown.js
@@ -16,7 +16,14 @@ window.onload = function(){
     canvas.height = WINDOW_HEIGHT;
 
     curShowTimeSeconds = getCurrentShowTimeSeconds()
-    render( context )
+    setInterval(
+        function(){
+            render( context );
+            update();
+        }
+        ,
+        50
+    );
 }
 
 function getCurrentShowTimeSeconds() {
@@ -27,7 +34,27 @@ function getCurrentShowTimeSeconds() {
     return ret >= 0 ? ret : 0;
 }
 
+function update(){
+
+    var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+
+    var nextHours = parseInt( nextShowTimeSeconds / 3600);
+    var nextMinutes = parseInt( (nextShowTimeSeconds - nextHours * 3600)/60 )
+    var nextSeconds = nextShowTimeSeconds % 60
+
+    var curHours = parseInt( curShowTimeSeconds / 3600);
+    var curMinutes = parseInt( (curShowTimeSeconds - curHours * 3600)/60 )
+    var curSeconds = curShowTimeSeconds % 60
+
+    if( nextSeconds != curSeconds ){
+
+        curShowTimeSeconds = nextShowTimeSeconds;
+    }
+}
+
 function render( cxt ){
+
+    cxt.clearRect(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
 
     var hours = parseInt( curShowTimeSeconds / 3600);
     var minutes = parseInt( (curShowTimeSeconds - hours * 3600)/60 )
